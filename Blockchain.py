1fcbefd3aecc8ce0241614334d4dd76f4b0f2183
Rishikesh Kadam <rishikesh1419@gmail.com>
2021-12-22 02:54:28 -0800

    Cleanup

--- Blockchain.py
+++ Blockchain.py
@@ -5,7 +5,7 @@ class Blockchain:
     head = None
     last_block = None
     chain = []
-    difficulty = 3
+    difficulty = 5
     prefix = "".join(["0" for _ in range(difficulty)])
 
     def __init__(self) -> None:
@@ -16,24 +16,17 @@ class Blockchain:
         temp = self.head
         while temp != None:
             string = string + "Data: " + temp.data + "\n"
-            string = string + "Hash: " + temp.hash + "\n"
+            string = string + "Hash: " + str(temp.hash) + "\n"
             string = string + "Nonce: " + str(temp.nonce) + "\n"
-            # print("Hash:", temp.hash)
-            # print("Next", temp.next)
             temp = temp.next
         return string
 
     def add_block(self, block: Block) -> None:
-        # print(block)
         self.last_block.next = block
         self.last_block = block
-        print(self.last_block)
 
     def mine_block(self, block: Block) -> None:
-        # nonce = 0
         hash = block.get_hash()
-        # print(hash[: self.difficulty])
-        # print(self.prefix)
         while hash[: self.difficulty] != self.prefix:
             block.nonce = block.nonce + 1
             hash = block.get_hash()
